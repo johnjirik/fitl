170883933bc425d53d51f224aaf3ee95a51a1488
John Jirik <jjirik@gmail.com>
2021-06-11 23:13:05 -0400

    did a database migration, but errors were generated because artisan recognized the same tables were in the fitl and homestead databases. I was only able to do a successful migration using unique tables names.

---
+++ database/migrations/2021_06_12_013421_create_newsarchives_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateNewsarchivesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('newsarchives', function (Blueprint $table) {
+            // $table->id();
+            // $table->increments('id');
+            // $table->string('title', 50);
+            $table->string('blah', 50);
+            // $table->string('headline');
+            $table->string('blahblah');
+            // $table->string('deck', 400)->nullable();
+            // $table->string('byline');
+            // $table->date('created_at');
+            // $table->string('dateline')->nullable();
+            // $table->text('body');
+            // $table->string('iso_three', 3);
+            // $table->string('url')->nullable();
+            // $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('newsarchives');
+    }
+}
